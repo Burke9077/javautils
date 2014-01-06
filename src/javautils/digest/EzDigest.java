c75a1cce0c84819252a85bb7aa539c518a612e75
Burke9077 <burke9077@gmail.com>
2014-01-06 17:51:12 -0500

    Added byte[] implementations

--- src/javautils/digest/EzDigest.java
+++ src/javautils/digest/EzDigest.java
@@ -10,74 +10,144 @@ import java.security.NoSuchAlgorithmException;
  */
 public class EzDigest {
     
-    public static String getMD2(byte[] _hash) throws NoSuchAlgorithmException {
+    public static String getMD2String(byte[] _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("MD2");
         return toHash(_hash, _digest);
     }
     
-    public static String getMD2(String _hash) throws NoSuchAlgorithmException {
+    public static byte[] getMD2(byte[] _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("MD2");
+        return _digest.digest(_hash);
+    }
+    
+    public static String getMD2String(String _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("MD2");
         return toHash(_hash, _digest);
     }
     
-    public static String getMD5(byte[] _hash) throws NoSuchAlgorithmException {
+    public static byte[] getMD2(String _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("MD2");
+        return _digest.digest(_hash.getBytes());
+    }
+    
+    public static String getMD5String(byte[] _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("MD5");
         return toHash(_hash, _digest);
     }
     
-    public static String getMD5(String _hash) throws NoSuchAlgorithmException {
+    public static byte[] getMD5(byte[] _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("MD5");
+        return _digest.digest(_hash);
+    }
+    
+    public static String getMD5String(String _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("MD5");
         return toHash(_hash, _digest);
     }
     
-    public static String getSHA(byte[] _hash) throws NoSuchAlgorithmException {
+    public static byte[] getMD5(String _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("MD5");
+        return _digest.digest(_hash.getBytes());
+    }
+    
+    public static String getSHAString(byte[] _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("SHA");
         return toHash(_hash, _digest);
     }
     
-    public static String getSHA(String _hash) throws NoSuchAlgorithmException {
+    public static byte[] getSHA(byte[] _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("SHA");
+        return _digest.digest(_hash);
+    }
+    
+    public static String getSHAString(String _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("SHA");
         return toHash(_hash, _digest);
     }
     
-    public static String getSHA1(byte[] _hash) throws NoSuchAlgorithmException {
+    public static byte[] getSHA(String _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("SHA");
+        return _digest.digest(_hash.getBytes());
+    }
+    
+    public static String getSHA1String(byte[] _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("SHA1");
         return toHash(_hash, _digest);
     }
     
-    public static String getSHA1(String _hash) throws NoSuchAlgorithmException {
+    public static byte[] getSHA1(byte[] _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("SHA1");
+        return _digest.digest(_hash);
+    }
+    
+    public static String getSHA1String(String _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("SHA1");
         return toHash(_hash, _digest);
     }
     
-    public static String getSHA256(byte[] _hash) throws NoSuchAlgorithmException {
+    public static byte[] getSHA1(String _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("SHA1");
+        return _digest.digest(_hash.getBytes());
+    }
+    
+    public static String getSHA256String(byte[] _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("SHA-256");
         return toHash(_hash, _digest);
     }
     
-    public static String getSHA256(String _hash) throws NoSuchAlgorithmException {
+    public static byte[] getSHA256(byte[] _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("SHA-256");
+        return _digest.digest(_hash);
+    }
+    
+    public static String getSHA256String(String _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("SHA-256");
         return toHash(_hash, _digest);
     }
     
-    public static String getSHA384(byte[] _hash) throws NoSuchAlgorithmException {
+    public static byte[] getSHA256(String _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("SHA-256");
+        return _digest.digest(_hash.getBytes());
+    }
+    
+    public static String getSHA384String(byte[] _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("SHA-384");
         return toHash(_hash, _digest);
     }
     
-    public static String getSHA384(String _hash) throws NoSuchAlgorithmException {
+    public static byte[] getSHA384(byte[] _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("SHA-384");
+        return _digest.digest(_hash);
+    }
+    
+    public static String getSHA384String(String _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("SHA-384");
         return toHash(_hash, _digest);
     }
     
-    public static String getSHA512(byte[] _hash) throws NoSuchAlgorithmException {
+    public static byte[] getSHA384(String _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("SHA-384");
+        return _digest.digest(_hash.getBytes());
+    }
+    
+    public static String getSHA512String(byte[] _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("SHA-512");
         return toHash(_hash, _digest);
     }
     
-    public static String getSHA512(String _hash) throws NoSuchAlgorithmException {
+    public static byte[] getSHA512(byte[] _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("SHA-512");
+        return _digest.digest(_hash);
+    }
+    
+    public static String getSHA512String(String _hash) throws NoSuchAlgorithmException {
         MessageDigest _digest = MessageDigest.getInstance("SHA-512");
         return toHash(_hash, _digest);
+    }
+    
+    public static byte[] getSHA512(String _hash) throws NoSuchAlgorithmException {
+        MessageDigest _digest = MessageDigest.getInstance("SHA-512");
+        return _digest.digest(_hash.getBytes());
     }
         
     /**
